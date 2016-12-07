58427aa3bfceaded07f64ca3d29ca65d45089784
Mathieu ROUSSEAU <mr.rousseaumathieu@gmail.com>
2016-12-07 21:35:57 +0100

    initialisation + creation du READEME.md + ajout du TP01

---
+++ README.md
@@ -0,0 +1,11 @@
+# formation DTA Java
+# author : Mathieu ROUSSEAU
+# email  : mr.rousseaumathieu@gmail.com
+
+
+## TP01 
+
+Exécution :
+	* d'un premier programme JAVA
+	* d'un programme avec un argument
+
---
+++ TP01/src/Bonjour.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mRousseau on 07/12/2016.
+ */
+public class Bonjour {
+
+    public static void main(String... args){
+        System.out.println("Bonjour tout le monde!");
+    }
+
+
+}
